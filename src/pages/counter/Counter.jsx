46ffdba16408d3b4350482c3bc0d5c466d41caf1
sayedmz <sayedmz9999@gmail.com>
2024-02-28 20:09:41 +0200

    initial commit

---
+++ src/pages/counter/Counter.jsx
@@ -0,0 +1,57 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import Header from "../../layout/header/Header";
+
+const Counter = () => {
+  const [value, setValue] = useState(5);
+
+  const [color, setColor] = useState("blue");
+  const increaseHandler = () => setValue(value + 1);
+  const increase10TimesHandler = () => setValue(value + 10);
+  const decreaseHandler = () => {
+    setValue(value - 1);
+  };
+  const inputHandler = (event) => {
+    setValue(+event.target.value);
+  };
+  const colorHandler = (e) => {
+    setColor(e.target.value);
+  };
+
+  return (
+    <>
+      <Header />
+      the number is :<strong> {value}</strong>
+      <Button
+        variant="contained"
+        size="small"
+        onClick={increaseHandler}
+        sx={{ backgroundColor: color }}
+      >
+        +1
+      </Button>
+      <Button
+        variant="contained"
+        size="small"
+        onClick={increase10TimesHandler}
+        sx={{ backgroundColor: color }}
+      >
+        +10
+      </Button>
+      <Button
+        variant="contained"
+        size="small"
+        onClick={decreaseHandler}
+        sx={{ backgroundColor: color }}
+      >
+        -1
+      </Button>
+      <input type="number" onChange={inputHandler} value={value} />
+      <input type="checkbox" />
+      <input type="date" />
+      <input type="color" onChange={colorHandler} value={color} />
+    </>
+  );
+};
+
+export default Counter;
